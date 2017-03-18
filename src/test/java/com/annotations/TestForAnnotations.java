03a20129b4ef10cd0a421714d62533c88b207de6
A <sindhu.1.a@on.nokia.com>
2017-03-18 19:50:18 +0530

    first commit

---
+++ src/test/java/com/annotations/TestForAnnotations.java
@@ -0,0 +1,11 @@
+package com.annotations;
+
+import org.testng.annotations.Test;
+
+public class TestForAnnotations {
+
+	@Test
+	public void TestCase(){
+				System.out.println("maven= project management tool");
+	}
+}
